44bf8a5cbaf78c404cdfb38ed9d46e0625a5048f
Vaaneesh <vaaneesh1483.be21@chitkara.edu.in>
2023-11-30 02:13:42 +0530

    changes made

--- static/index.js
+++ static/index.js
@@ -12,22 +12,33 @@ form.addEventListener("submit",(ev)=>{
         console.log(data);
         input.value="";
         let div=document.createElement("div");
-        div.innerHTML=`${taskitem}<button class="edit-btn">Edit</button> <button class="delete-btn">Delete</button>`;
+        div.innerHTML=`<span class="task-item"><span class="circle"></span>${taskitem}</span><button class="edit-btn"><i class="fa-solid fa-user-pen"></i></button> <button class="delete-btn">X</button>`;
         tasklist.append(div);
+
+        div.querySelector(".task-item").addEventListener("click",()=>{
+            div.querySelector(".task-item").classList.toggle("completed");
+        })
+
         div.querySelector(".edit-btn").addEventListener("click",async()=>{
             editTask(taskitem,div);
         })
+
         div.querySelector(".delete-btn").addEventListener("click",async()=>{
             await deletetask(taskitem,div);
         })
+
     })
 })
 
 function showData(data){
     data.forEach(task => {
         let div=document.createElement("div");
-        div.innerHTML=`${task}<button class="edit-btn">Edit</button> <button class="delete-btn">Delete</button>`;
+        div.innerHTML=`<span class="task-item"><span class="circle"></span>${task}</span><button class="edit-btn"><i class="fa-solid fa-user-pen"></i></button> <button class="delete-btn">X</button>`;
         tasklist.append(div);
+
+        div.querySelector(".task-item").addEventListener("click",()=>{
+            div.querySelector(".task-item").classList.toggle("completed");
+        })
 
         div.querySelector(".edit-btn").addEventListener("click",async()=>{
             editTask(task,div);
@@ -69,22 +80,27 @@ function editTask(task,div){
         });
 
         // taskElement.textContent=newTaskname;
-        const updatedDiv=document.createElement("div");
-        updatedDiv.innerHTML=`${newTaskname} <button class="edit-btn">Edit</button><button class="delete-btn">Delete</button>`;
-        inputField.replaceWith(updatedDiv);
+        const updatedDiv = div.cloneNode(true); //remove existing event listeners
+        updatedDiv.innerHTML=`<span class="task-item"><span class="circle"></span>${newTaskname}</span><button class="edit-btn"><i class="fa-solid fa-user-pen"></i></button><button class="delete-btn">X</button>`;
+        div.replaceWith(updatedDiv);
         
         updatedDiv.querySelector(".edit-btn").addEventListener("click",async()=>{
-            editTask(task,updatedDiv);
+            editTask(newTaskname,updatedDiv);
         })
 
         updatedDiv.querySelector(".delete-btn").addEventListener("click",async()=>{
-            await deletetask(task,updatedDiv);
+            await deletetask(newTaskname,updatedDiv);
         })
+
+        updatedDiv.querySelector(".task-item").addEventListener("click",()=>{
+            updatedDiv.querySelector(".task-item").classList.toggle("completed");
+        })
+        
     })
     inputField.focus();
 }
 async function getdata(Api){
-    let data=await fetch(`http://localhost:3333${Api}`);
+    let data=await fetch(Api);
     let responsedata=await data.json();
     console.log(responsedata);
     showData(responsedata);
